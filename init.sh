8e1359c6f0e9c9d0b2a79f75390cac787f9b7a8b
artysleep <arty.sleep@gmail.com>
2024-02-26 16:49:27 +0300

    example_env added and init.sh changed

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 
-echo MAIN_HOST_IP=$(hostname -i | awk '{print $1}') >> .env
+sed '/MAIN_HOST_IP/,/DNS_IP/d' .env > .env2
+echo MAIN_HOST_IP=$(hostname -i | awk '{print $1}') >> .env2
+echo "DNS_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')" >> .env2
+rm -f .env
+mv .env2 .env
